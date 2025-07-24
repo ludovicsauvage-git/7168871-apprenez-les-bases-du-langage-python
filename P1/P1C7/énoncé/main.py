412733fe849da5fb1aa532ba08d877d9a04a91dd
lsauvage <lsauvage@ik.me>
2025-07-24 22:35:05 +0200

    fin exercice P1C7

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+
+fruits = {
+    "pomme":"rouge",
+    "banane":"jaune",
+    "orange":"orange"
+}
+print(fruits)
+fruits["kiwi"] = "vert"
+print(fruits)
+couleur_banane = fruits["banane"]
+print(couleur_banane)
+fruits["pomme"] = "vert"
+print(fruits)
+del fruits["banane"]
+print(fruits)
+print(fruits.keys())
