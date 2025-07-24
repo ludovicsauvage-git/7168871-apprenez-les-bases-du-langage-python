8b18bb741609a197ecc2713a6810d1dcd38df9ec
lsauvage <lsauvage@ik.me>
2025-07-24 20:28:53 +0200

    Exerice 2 fini

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,12 @@
 # Écrivez votre code ici !
+#Variable "nom"
+nom = "Ludovic"
+#Variable "age"
+age = 26
+
+# commande pour faire apparaître je m'appelle Ludovic et j'ai 26 ans.
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+# Modification de la variable age
+age = age + 10  #ou "age += 10"
+# commande pour faire apparaître je m'appelle Ludovic et j'ai 36 ans.
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant.")
