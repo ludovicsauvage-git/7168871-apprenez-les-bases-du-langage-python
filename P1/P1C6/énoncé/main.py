51d743b3342a08448322bff2307f786bb6590f8c
lsauvage <lsauvage@ik.me>
2025-07-24 22:14:43 +0200

    Fin exercice P1C6

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+
+fruits = ["pomme","banane","orange"]
+print(fruits)
+fruits.append("kiwi")
+print(fruits)
+fruits.remove("orange")
+print(fruits)
+fruits[1] = "ananas"
+print(fruits) 
+print(len(fruits))
+fruits.sort()
+print(fruits)
