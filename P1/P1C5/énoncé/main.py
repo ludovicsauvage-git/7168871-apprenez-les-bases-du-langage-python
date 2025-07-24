4d2f42549fff4da9cef84414fc6de7a0a1a50dcf
lsauvage <lsauvage@ik.me>
2025-07-24 21:45:19 +0200

    Exercice P1C5

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+
+nom = "Ludovic"
+age = 26
+taille = 1.92
+est_etudiant = True
+
+print(nom)
+print(age)
+print(f" {taille} mètres.")
+print(est_etudiant)
+
+# Afficher les types des quatres variables
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
